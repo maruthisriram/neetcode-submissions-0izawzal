8d75899f90a71585cebd6e5eaec2c6e4665ea7e1
maruthisriram <56600730+maruthisriram@users.noreply.github.com>
2026-09-21 17:46:04 +0530

    Add: word-break - submission-2

---
+++ Data Structures & Algorithms/word-break/submission-2.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordSet = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true; // Base case: empty string
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break; // Stop checking other splits once dp[i] is true
+                }
+            }
+        }
+
+        return dp[s.length()];
+    }
+}
